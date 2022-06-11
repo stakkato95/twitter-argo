0406a3c921dccd0fdea2d894edf143c4e719c744
Artsiom Kaliaha <stakkato95@gmail.com>
2022-06-11 20:12:55 +0200

    Add script to delete postgres and mysql pvc

--- app/app-uninstall.ps1
+++ app/app-uninstall.ps1
@@ -1,1 +1,11 @@
 argocd app delete twitter-app
+
+$mysqlPvc = kubectl get pvc -l app.kubernetes.io/name=mysql -l app.kubernetes.io/instance=twitter-app -o=name
+if ($mysqlPvc) {
+    kubectl delete $($mysqlPvc)
+}
+
+$postgresqlPvc = kubectl get pvc -l app.kubernetes.io/name=postgresql -l app.kubernetes.io/instance=twitter-app -o=name
+if ($postgresqlPvc) {
+    kubectl delete $($postgresqlPvc)
+}
