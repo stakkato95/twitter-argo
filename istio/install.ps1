fa21c13d43fbdd94aebbb25610366580985c5043
Artsiom Kaliaha <stakkato95@gmail.com>
2022-06-17 08:47:15 +0200

    Add automatic annotation of default namespace on istio installation

--- istio/install.ps1
+++ istio/install.ps1
@@ -1,3 +1,5 @@
+echo "`nadd label to default namespace...`n"
+kubectl label ns default istio-injection=enabled
 echo "`n1-istio-init...`n"
 kubectl apply -f 1-istio-init.yaml
 echo "`n2-istio-minikube...`n"
